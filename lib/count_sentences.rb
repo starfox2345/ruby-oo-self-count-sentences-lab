3debf29bf9252becb07081280625d2271700ffdd
yatsun <yatsunlam25@gmail.com>
2021-03-15 19:09:19 -0400

    commit tree aa7b7b69af77f235159f555ee454c385e118ef05

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,31 @@ require 'pry'
 class String
 
   def sentence?
-    
+   self.end_with?(".")
+    #   true
+    # else
+    #   false
+    # end
+
+    # if "Hi, my name is Sophie.".end_with?(".")
+    #     true
+    # elsif "Hi, my name is Sophie".end_with?(".")
+    #     false
+    # else
+    #     false
+    # end
+
   end
 
   def question?
-
+    self.end_with?("?")
   end
 
   def exclamation?
-
+    self.end_with?("!")
   end
 
   def count_sentences
-
+    self.squeeze("!!").split(/[.!?]/).count
   end
 end
